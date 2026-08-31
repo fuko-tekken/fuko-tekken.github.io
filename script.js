116836e211638edd92b2b4d510a4facd1e5233f8
fuko-tekken <fuko.tekken@gmail.com>
2026-08-31 17:19:47 +0900

    booksを大幅改造

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 function toggleMenu(element) {
   element.classList.toggle("active");
 }
+
+async function countUp(event, link, name) {
+  event.preventDefault();
+
+  const data = new URLSearchParams();
+  data.append("name", name);
+
+  await fetch("https://script.google.com/macros/s/AKfycbwISysNe1a6ftaQIyogNOPHi4af0tiodFv_CiNKUOwGAfApfVamkjuOCIR9D28vbIrzzQ/exec", {
+      method: "POST",
+      body: data
+  });
+
+  location.href = link.href;
+}
